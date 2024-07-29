1dc56730287ea3adc59c5b0b6597bd78e0303e17
JeremyArc <jessada.srm@gmail.com>
2024-07-29 09:49:10 +0700

    change vpc output variables naming

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,14 +1,14 @@
-output "NETWORK_NAME" {
+output "network_name" {
   value = module.vpc.network_name
 }
 
-output "PRIVATE_SUBNET_NAME" {
+output "private_subnet_name" {
   value = module.vpc.subnets["asia-southeast1/private-subnet-1"].name
 }
 
-output "PRIVATE_SUBNET_IP_RANGE_POD" {
+output "private_subnet_ip_range_pod" {
   value = module.vpc.subnets_secondary_ranges[0][0].range_name
 }
-output "PRIVATE_SUBNET_IP_RANGE_SERVICES" {
+output "private_subnet_ip_range_services" {
   value = module.vpc.subnets_secondary_ranges[0][1].range_name
 }
